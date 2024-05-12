4168087ff75befe35b7fae953dc193df7cb6755e
babygrandad <relebogilenkotswe@gmail.com>
2024-05-12 15:10:56 +0200

    format files

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-def print_arg(argv):
-    n = len(argv) - 1
-    if n == 0:
-        print("{:d} argument.".format(n))
-        return
-    else:
-        if n == 1:
-            print("{:d} argument:".format(n))
-        else:
-            print("{:d} arguments:".format(n))
-        i = 1
-        while i <= n:
-            print("{:d}: {:s}".format(i, argv[i]))
-            i += 1
-
 if __name__ == "__main__":
     import sys
-    print_arg(sys.argv)
+    a = len(sys.argv)
+    if a == 1:
+        print("{:d} arguments.".format(a - 1))
+    elif a == 2:
+        print("{:d} argument:".format(a - 1))
+        print("{:d}: {:s}".format(a - 1, sys.argv[1]))
+    else:
+        print("{:d} arguments:".format(a - 1))
+        for i in range(a):
+            if i == 0:
+                continue
+            print("{:d}: {:s}".format(i, sys.argv[i]))
