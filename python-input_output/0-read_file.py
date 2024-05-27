94df168dac051393d3992ef4ac720fb7e8c9b38f
babygrandad <relebogilenkotswe@gmail.com>
2024-05-27 08:28:02 +0200

    fix file 0

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""
-a function that reads a text file (UTF8) and prints it to stdout
-"""
+"""define function read"""
 
 
 def read_file(filename=""):
-    """
-    Reads a text file (UTF8) and prints its content to stdout
-
-    filename (str): The name of the file to read
-    """
-    with open(filename, mode="r", encoding="utf-8") as file:
-        for line in file:
-            print(line, end="")
+    """read file function"""
+    with open(filename, encoding="utf-8") as f:
+        f = (f.read()[:-1])
+        if f == "":
+            return
+        else:
+            print(f)
