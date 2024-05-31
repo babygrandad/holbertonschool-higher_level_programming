1822c989ae96d3424987cc88a5525545747834a0
babygrandad <relebogilenkotswe@gmail.com>
2024-05-31 09:38:16 +0200

    fix last endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -33,12 +33,10 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
         
         else:
             self.send_response(404)
-            self.send_header('Content-type', 'application/json')
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')
             self.end_headers()
-            response = {
-                "error": "Endpoint not found"
-            }
-            self.wfile.write(json.dumps(response).encode('utf-8'))
+            self.wfile.write(b"404 Not Found")
 
 def run(server_class=HTTPServer, handler_class=SimpleHTTPRequestHandler, port=8000):
     server_address = ('', port)
