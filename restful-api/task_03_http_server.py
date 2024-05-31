f0d76bfc7ec8ffbfd4e396cdc3b48536e52ac11e
babygrandad <relebogilenkotswe@gmail.com>
2024-05-31 09:42:33 +0200

    fix last endpoint

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -22,12 +22,13 @@ class SimpleHTTPRequestHandler(BaseHTTPRequestHandler):
             }
             self.wfile.write(json.dumps(response).encode('utf-8'))
         
-        elif self.path == '/status':
+        elif self.path == '/info':  # Make sure this matches the expected endpoint
             self.send_response(200)
             self.send_header('Content-type', 'application/json')
             self.end_headers()
             response = {
-                "version": "1.0", "description": "A simple API built with http.server"
+                "version": "1.0",
+                "description": "A simple API built with http.server"
             }
             self.wfile.write(json.dumps(response).encode('utf-8'))
         
